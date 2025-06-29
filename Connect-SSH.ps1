fd77f4d3ad0dbf3fa37e77426275cb88b42fdbaf
Isma <xwarzmiixz@gmail.com>
2025-06-29 21:35:32 +0200

    CHANGE: Personal custom profile

--- Connect-SSH.ps1
+++ Connect-SSH.ps1
@@ -1,7 +1,8 @@
-$target = $env:SSH_TARGET
+$target = $Env:SSH_TARGET
+if (-not $target) { Write-Error 'SSH_TARGET is not defined'; exit 1 }
 
-Start-Process pwsh @(
-  '-NoExit',
-  '-Command',
-  "ssh $target"
+Start-Process wt.exe `
+-ArgumentList @(
+  '--profile', 'PowerShell',
+  'pwsh', '-NoExit', '-Command', "ssh $target"
 )
